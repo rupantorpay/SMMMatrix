2b3610dabf7370134f558a4208004749bb1bf67a
nexavoiddev <nexavoiddev@gmail.com>
2025-04-20 21:22:35 +0600

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO `gateways` (`id`, `code`, `name`, `parameters`, `currencies`, `extra_parameters`, `currency`, `symbol`, `min_amount`, `max_amount`, `percentage_charge`, `fixed_charge`, `convention_rate`, `sort_by`, `image`, `status`, `note`, `created_at`, `updated_at`) VALUES ('50', 'rupantorpay', 'BD Automatic Payment', '{\"api_key\":\"\"}', '{\"0\":{\"BDT\":\"BDT\",\"USD\":\"USD\"}}', NULL, 'BDT', 'BDT', '1.00000000', '10000.00000000', '1.0000', '0.50000000', '110', '4', '6312ffa2e559012189474.jpg', '0', NULL, '2020-09-10 09:05:02', '2023-08-16 16:18:30');
